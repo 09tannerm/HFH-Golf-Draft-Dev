ad42af0619f54534ac5876c5b85e92b705f369e5
09tannerm <09tannerm@gmail.com>
2025-04-24 18:33:59 -0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // ✅ Fixes the 404 JS asset error on Vercel
+  base: './',
   plugins: [react()],
-})
+});
