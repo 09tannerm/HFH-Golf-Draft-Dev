fca724a56ae87c1759089979e9cb92668e707b3f
09tannerm <09tannerm@gmail.com>
2025-04-24 19:05:59 -0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>HFH Golf Draft</h1>
+      <p>Welcome. Everything is working as expected 🎯</p>
+    </div>
+  );
+}
