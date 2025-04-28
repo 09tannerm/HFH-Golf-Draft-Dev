7a1cd1ede8cfabeb9f082d57539ccb359cee7a69
09tannerm <09tannerm@gmail.com>
2025-04-28 11:17:04 -0600

    Delete src directory

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-
-export const firebaseConfig = {
-  apiKey: "your-api-key-here",
-  authDomain: "your-auth-domain-here",
-  projectId: "your-project-id-here",
-  storageBucket: "your-storage-bucket-here",
-  messagingSenderId: "your-messaging-sender-id-here",
-  appId: "your-app-id-here"
-};
